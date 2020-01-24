c9c8aa76f9b5b5ad8f567b395da2fc8c585c356d
Ryan Drachenberg <tssinvestments@gmail.com>
2020-01-24 12:34:16 -0500

    second commit, html and routing working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,12 @@
-
+// DEPENDENCIES
+var path = require("path");
 module.exports = function (app) {
     app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
